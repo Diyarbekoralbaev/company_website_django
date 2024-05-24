3b2378900b364c9a0a4a62c34fef193b0318ca4f
Diyarbek Oralbaev <diyarbekoralbaev@gmail.com>
2024-05-24 15:40:30 +0500

    Update settings.py

--- company_website_django/settings.py
+++ company_website_django/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!itx37e93wekgno3z1%oo_oxug$)gaz-lztha!lno3gf@z&h^o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
